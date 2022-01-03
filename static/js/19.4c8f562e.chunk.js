61166cb0a99bcb34b3e32b43369ee1d601a57e0e
CryptoPoker <91441851+Crypto-Poker@users.noreply.github.com>
2022-01-03 15:56:20 +0800

    Delete static directory

--- static/js/19.4c8f562e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[19],{2270:function(p,s){}}]);
-//# sourceMappingURL=19.4c8f562e.chunk.js.map
--- static/js/20.839cc1b8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[20],{2129:function(p,s){}}]);
-//# sourceMappingURL=20.839cc1b8.chunk.js.map
